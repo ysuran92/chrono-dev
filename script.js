7fd098d3bff33f88b8d588aa5f8f4c90ba8805fa
ysuran92 <martinyanez420@gmail.com>
2020-08-29 07:54:28 -0300

    test popups 2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+let popup = document.createElement('div');
+let cancel = document.createElement('div');
+cancel.className = 'cancel';
+cancel.innerHTML = 'close';
+cancel.onclick = function(e) { popup.parentNode.removeChild(popup) };
+let message = document.createElement('span');
+
 function popUpCrono() {
-    let popup = document.createElement('div');
     popup.className = 'popupCrono';
     popup.id = 'popupCrono';
-    let cancel = document.createElement('div');
-    cancel.className = 'cancel';
-    cancel.innerHTML = 'close';
-    cancel.onclick = function(e) { popup.parentNode.removeChild(popup) };
-    let message = document.createElement('span');
     message.innerHTML = "This is a Crono message";
     popup.appendChild(message);
     popup.appendChild(cancel);
@@ -14,14 +15,8 @@ function popUpCrono() {
 }
 
 function popUpLucca() {
-    let popup = document.createElement('div');
     popup.className = 'popupLucca';
     popup.id = 'popupLucca';
-    let cancel = document.createElement('div');
-    cancel.className = 'cancel';
-    cancel.innerHTML = 'close';
-    cancel.onclick = function(e) { popup.parentNode.removeChild(popup) };
-    let message = document.createElement('span');
     message.innerHTML = "This is a Lucca message";
     popup.appendChild(message);
     popup.appendChild(cancel);
