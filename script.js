edef1284107b29c89546e6fe8d409cf9ea332d21
ysuran92 <martinyanez420@gmail.com>
2020-08-29 07:52:59 -0300

    test popups

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function popUpCrono() {
-    var popup = document.createElement('div');
+    let popup = document.createElement('div');
     popup.className = 'popupCrono';
     popup.id = 'popupCrono';
-    var cancel = document.createElement('div');
+    let cancel = document.createElement('div');
     cancel.className = 'cancel';
     cancel.innerHTML = 'close';
     cancel.onclick = function(e) { popup.parentNode.removeChild(popup) };
-    var message = document.createElement('span');
-    message.innerHTML = "This is a test message";
+    let message = document.createElement('span');
+    message.innerHTML = "This is a Crono message";
+    popup.appendChild(message);
+    popup.appendChild(cancel);
+    document.body.appendChild(popup);
+}
+
+function popUpLucca() {
+    let popup = document.createElement('div');
+    popup.className = 'popupLucca';
+    popup.id = 'popupLucca';
+    let cancel = document.createElement('div');
+    cancel.className = 'cancel';
+    cancel.innerHTML = 'close';
+    cancel.onclick = function(e) { popup.parentNode.removeChild(popup) };
+    let message = document.createElement('span');
+    message.innerHTML = "This is a Lucca message";
     popup.appendChild(message);
     popup.appendChild(cancel);
     document.body.appendChild(popup);
